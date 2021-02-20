5b0cb54ee5e8ff87e2305ad90f0ba86987f7a438
Rolando <rolando.fcfm2020@gmail.com>
2021-02-20 16:28:06 -0600

    first commit

---
+++ GotoRemover/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GotoRemover
+{
+    class Program
+    {
+
+        public static void printFor()
+        {
+            DoLoop();
+
+            Console.WriteLine("Terminé");
+
+        }
+
+        private static void DoLoop()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+
+                    Console.WriteLine("Lol");
+                    if (j == 3)
+                    {
+                        //goto endloop
+                        return;
+                    }
+                }
+            }
+            //endloop:
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    //goto finish
+                    break;
+                }
+                Console.WriteLine("i value : {0}", i);
+            }
+
+            //finish:
+            Console.WriteLine("The end");
+
+            printFor();
+            Console.ReadLine();
+        }
+    }
+}
